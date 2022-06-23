b02626802b79a521656e178943c85552a54b87de
Byron Dunkley <ByronDunkley@Shans-MacBook-Pro-2.local>
2022-06-23 23:20:21 +0100

    update remix config file

--- remix.config.js
+++ remix.config.js
@@ -7,13 +7,14 @@ module.exports = {
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: ["**/.*"],
+  //ignoredRouteFiles: ["**/.*"],
   //  appDirectory: "app",
   //  assetsBuildDirectory: "public/build",
   //   serverBuildPath: "api/index.js",
   //  publicPath: "/build/",
   appDirectory: "app",
-  browserBuildDirectory: "public/build",
+  assetsBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildDirectory: "api/_build"
+  serverBuildDirectory: "api/_build",
+  ignoredRouteFiles: [".*"]
 };
